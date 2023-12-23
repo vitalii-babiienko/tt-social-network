725276a2f32e7312a88fd487c2818fc8d5e2df90
Vitalii Babiienko <v.babiyenko@gmail.com>
2023-12-23 14:59:09 +0200

    Implement the project throttling

--- social_network/settings.py
+++ social_network/settings.py
@@ -133,3 +133,15 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "100/day",
+        "user": "1000/day",
+    },
+}

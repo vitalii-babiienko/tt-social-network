3ff1ea92294952e88c01804a645e93e6f0c0ffa5
Vitalii Babiienko <v.babiyenko@gmail.com>
2023-12-24 18:37:21 +0200

    Register routes for the post app

--- post/urls.py
+++ post/urls.py
@@ -1,3 +1,14 @@
-urlpatterns = []
+from rest_framework import routers
+
+from post.views import (
+    HashtagViewSet,
+    PostViewSet,
+)
+
+router = routers.DefaultRouter()
+router.register("hashtags", HashtagViewSet)
+router.register("", PostViewSet)
+
+urlpatterns = router.urls
 
 app_name = "post"

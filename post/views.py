46459d2534e08799711cedd51e3369cd68a167a4
Vitalii Babiienko <v.babiyenko@gmail.com>
2023-12-24 18:36:31 +0200

    Add viewsets HashtagViewSet and PostViewSet to the views.py

--- post/views.py
+++ post/views.py
@@ -1,3 +1,211 @@
-from django.shortcuts import render
+from django.db.models import Count
+from drf_spectacular.utils import extend_schema, OpenApiParameter
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from post.models import (
+    Hashtag,
+    Post,
+)
+from post.paginations import (
+    HashtagPagination,
+    PostPagination,
+)
+from post.permissions import (
+    IsPostOwnerOrReadOnly,
+)
+from post.serializers import (
+    HashtagSerializer,
+    PostSerializer,
+    PostListSerializer,
+    PostDetailSerializer,
+    PostImageSerializer,
+    CommentAddSerializer,
+)
+
+
+class UploadImageMixin:
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="upload-image",
+    )
+    def upload_image(self, request, pk=None):
+        """Endpoint for uploading the image to the specific instance"""
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@extend_schema(tags=["Hashtags"])
+class HashtagViewSet(viewsets.ModelViewSet):
+    queryset = Hashtag.objects.all()
+    serializer_class = HashtagSerializer
+    permission_classes = (IsAuthenticated,)
+    pagination_class = HashtagPagination
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+        if self.action == "list":
+            name = self.request.query_params.get("name")
+
+            if name:
+                queryset = queryset.filter(name__icontains=name)
+
+        return queryset
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="name",
+                type=str,
+                description="Filter by name (ex. ?name=FREEDOM)",
+                required=False,
+            ),
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+
+@extend_schema(tags=["Posts"])
+class PostViewSet(UploadImageMixin, viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (IsAuthenticated, IsPostOwnerOrReadOnly)
+    pagination_class = PostPagination
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+        if self.action == "list":
+            title = self.request.query_params.get("title")
+            author = self.request.query_params.get("author")
+
+            if title:
+                queryset = queryset.filter(title__icontains=title)
+
+            if author:
+                queryset = queryset.filter(author__username__icontains=author)
+
+        queryset = (
+            queryset
+            .select_related("author")
+            .prefetch_related(
+                "hashtags",
+                "comments__author",
+            )
+            .annotate(
+                likes_count=Count("likes", distinct=True),
+                comments_count=Count("comments", distinct=True),
+            )
+            .order_by("-created_at")
+        )
+
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action in (
+            "list",
+            "show_favorite_posts",
+        ):
+            return PostListSerializer
+
+        if self.action == "retrieve":
+            return PostDetailSerializer
+
+        if self.action == "upload_image":
+            return PostImageSerializer
+
+        if self.action == "add_comment":
+            return CommentAddSerializer
+
+        return PostSerializer
+
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="like-unlike",
+        permission_classes=[IsAuthenticated],
+    )
+    def like_unlike_post(self, request, pk=None):
+        """Endpoint for liking/unliking posts feature"""
+        post = self.get_object()
+        user = self.request.user
+
+        if user not in post.likes.all():
+            post.likes.add(user)
+            return Response(
+                {"detail": "You have successfully liked the post."},
+                status=status.HTTP_200_OK,
+            )
+
+        post.likes.remove(user)
+        return Response(
+            {"detail": "Your like was successfully removed."},
+            status=status.HTTP_200_OK,
+        )
+
+    @action(
+        methods=["GET"],
+        detail=False,
+        url_path="favorite",
+        permission_classes=[IsAuthenticated],
+    )
+    def show_favorite_posts(self, request):
+        """Endpoint for showing a list of the favorite posts"""
+        user = self.request.user
+        posts = Post.objects.filter(likes=user)
+        serializer = self.get_serializer(posts, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="add-comment",
+        permission_classes=[IsAuthenticated],
+    )
+    def add_comment(self, request, pk=None):
+        """Endpoint for adding a comment to the post"""
+        user = self.request.user
+        post = self.get_object()
+        serializer = CommentAddSerializer(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+        serializer.save(
+            author=user,
+            post=post,
+            content=serializer.validated_data["content"],
+        )
+        return Response(
+            {
+                "detail": "Your comment has been successfully "
+                          "added to the post."
+            },
+            status=status.HTTP_200_OK,
+        )
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="title",
+                type=str,
+                description="Filter by title (ex. ?title=Numpy)",
+                required=False,
+            ),
+            OpenApiParameter(
+                name="author",
+                type=str,
+                description="Filter by author (ex. ?author=Jack)",
+                required=False,
+            ),
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)

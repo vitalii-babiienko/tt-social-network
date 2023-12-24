3fa81d9f09a01de3999367c77b12a4892dc9f140
Vitalii Babiienko <v.babiyenko@gmail.com>
2023-12-24 18:09:03 +0200

    Add MEDIA_ROOT and MEDIA_URL to the settings.py

--- social_network/settings.py
+++ social_network/settings.py
@@ -135,6 +135,9 @@ USE_TZ = False
 
 STATIC_URL = "static/"
 
+MEDIA_ROOT = "/vol/web/media"
+MEDIA_URL = "/media/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 

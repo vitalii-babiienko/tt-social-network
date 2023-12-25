efe6bddce7c338ed2219b806a69691ba040d6a1b
Vitalii Babiienko <v.babiyenko@gmail.com>
2023-12-25 11:01:17 +0200

    Add the analytics endpoint

--- post/urls.py
+++ post/urls.py
@@ -1,14 +1,18 @@
+from django.urls import path
 from rest_framework import routers
 
 from post.views import (
     HashtagViewSet,
     PostViewSet,
+    LikesAnalyticsView,
 )
 
 router = routers.DefaultRouter()
 router.register("hashtags", HashtagViewSet)
 router.register("", PostViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path("analytics/", LikesAnalyticsView.as_view(), name="analytics"),
+] + router.urls
 
 app_name = "post"
